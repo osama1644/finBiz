a60f97ee78bf48f6fbab4efd05c53d00e9e54228
osama1644 <latifosama605@gmail.com>
2025-12-11 11:34:00 +0200

    fixing links

--- src/_components/Links/NavLinks.tsx
+++ src/_components/Links/NavLinks.tsx
@@ -2,13 +2,15 @@ import styles from "./Links.module.css"
 function NavLinks() {
   return (
     <div className={styles.links}>
-        <ul>
+       <div className={styles.link}>
+         <ul>
           <li>Product</li>
           <li>Integration</li>
           <li>Demo</li>
           <li>Pricing</li>
           <li>Login</li>
         </ul>
+       </div>
     </div>
   )
 }
